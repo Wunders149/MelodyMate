87de302ccd724f8d7364356d8ac1e39689222887
Wunders149 <razafimahefaphilibert7@gmail.com>
2024-08-04 16:57:21 +0300

    Adding navBar

---
+++ src/scripts/scripts.js
@@ -0,0 +1,14 @@
+function chargerPage(url, conteneurId) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.getElementById(conteneurId).innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    chargerPage('navbar.html', 'conteneur');
+});
