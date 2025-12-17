a138bac86265919794ee0cde4ec5010caa97302b
Wunders149 <razafimahefaphilibert7@gmail.com>
2025-12-17 16:52:12 +0300

    Update

---
+++ dist/search.c719088e.js
@@ -0,0 +1,18 @@
+document.getElementById('lyricsForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    let artist = document.getElementById('artist').value.trim();
+    let title = document.getElementById('title').value.trim();
+    if (artist === '' || title === '') {
+        alert('Please fill the field!');
+        return;
+    }
+    fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`).then((response)=>response.json()).then((data)=>{
+        if (data.lyrics) document.getElementById('lyrics').innerText = data.lyrics;
+        else document.getElementById('lyrics').innerText = 'Lyrics not found';
+    }).catch((error)=>{
+        document.getElementById('lyrics').innerText = 'An Error occured! Please try again later';
+        console.error('Erreur:', error);
+    });
+});
+
+//# sourceMappingURL=search.c719088e.js.map
